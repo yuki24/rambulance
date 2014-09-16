b529b197569f29abb6637ed934c220b0039df3fe
Yuki Nishijima <mail@yukinishijima.net>
2014-09-15 20:04:50 -0700

    Make spec descriptions slightly better

--- spec/requests/error_json_spec.rb
+++ spec/requests/error_json_spec.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 require 'spec_helper'
 
-feature 'Error json responses', if: !ENV["CUSTOM_EXCEPTIONS_APP"], type: :request do
+feature 'Error json response', if: !ENV["CUSTOM_EXCEPTIONS_APP"], type: :request do
   def json_response
     JSON.parse(response.body)
   end
@@ -10,28 +10,28 @@ feature 'Error json responses', if: !ENV["CUSTOM_EXCEPTIONS_APP"], type: :reques
     super(path, nil, "CONTENT_TYPE" => "application/json", "HTTP_ACCEPT" => "application/json")
   end
 
-  scenario 'Unprocessable entity due to ActionController:InvalidAuthenticityToken but without its template' do
+  scenario 'returns 422 json due to ActionController:InvalidAuthenticityToken but without its template' do
     get '/users/new'
 
     expect(response.status).to eq(422)
     expect(json_response['message']).to eq("Something went wrong")
   end
 
-  scenario 'Internal server error due to RuntimeError' do
+  scenario 'returns 500 json due to RuntimeError' do
     get '/users/1.json'
 
     expect(response.status).to eq(500)
     expect(json_response['message']).to eq("Something went wrong")
   end
 
-  scenario 'Not found due to CustomException' do
+  scenario 'returns 404 json due to CustomException' do
     get '/users.json'
 
     expect(response.status).to eq(404)
     expect(json_response['message']).to eq("Page not found")
   end
 
-  scenario 'Not found due to ActinoController::RoutingError' do
+  scenario 'returns 404 json due to ActinoController::RoutingError' do
     get '/doesnt_exist.json'
 
     expect(response.status).to eq(404)
