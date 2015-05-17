7bc189c78d74aee4d47b017a2636df9cd7dda6c8
Yuki Nishijima <mail@yukinishijima.net>
2015-05-17 15:32:58 -0400

    Simplify Railtie

--- lib/rambulance/railtie.rb
+++ lib/rambulance/railtie.rb
@@ -1,25 +1,20 @@
 module Rambulance
   class Railtie < Rails::Railtie
-    initializer "action_controller" do |app|
-      require "rambulance/exceptions_app"
-    end
-
-    initializer "activesupport.dependencies" do |app|
-      begin
-        ActiveSupport::Dependencies.load_missing_constant(Object, :ExceptionsApp)
-        app.config.exceptions_app = ->(env){ ::ExceptionsApp.call(env) }
-      rescue NameError
-        app.config.exceptions_app = ::Rambulance::ExceptionsApp
-      end
-    end
-
     initializer 'rambulance' do |app|
-      ActiveSupport.on_load(:after_initialize) do
-        if Rails.env.development?
-          Rails.application.routes.append do
-            mount app.config.exceptions_app, at: '/rambulance'
-          end
+      require "rambulance/exceptions_app"
+
+      app.config.exceptions_app =
+        begin
+          ActiveSupport::Dependencies.load_missing_constant(Object, :ExceptionsApp)
+          ->(env){ ::ExceptionsApp.call(env) }
+        rescue NameError
+          ::Rambulance::ExceptionsApp
         end
+
+      ActiveSupport.on_load(:after_initialize) do
+        Rails.application.routes.append do
+          mount app.config.exceptions_app, at: '/rambulance'
+        end if Rails.env.development?
       end
     end
   end
