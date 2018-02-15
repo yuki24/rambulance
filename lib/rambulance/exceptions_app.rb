2e4ff7f170f601278ea3ba998db84cbb61ed40ec
JoKer <joker777vs.666@gmail.com>
2018-02-15 16:23:33 -0500

    Uses html mime type object instead of symbol (#41)
    https://github.com/yuki24/rambulance/issues/41

--- lib/rambulance/exceptions_app.rb
+++ lib/rambulance/exceptions_app.rb
@@ -66,7 +66,7 @@ module Rambulance
       end
 
       # The #format method needs to be called after the sanitization above.
-      request.formats = request.formats.map(&:ref) << :html
+      request.formats << Mime::Type.register("text/html", :html)
 
       super
     end
