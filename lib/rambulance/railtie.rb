d5a683b3bad51e4d2df7f8146c75673c51bcdd9b
Yuki Nishijima <yk.nishijima@gmail.com>
2019-09-30 22:42:31 -0400

    Fixes a bug where custom exceptions app does not load on Rails 6
    closes #52

--- lib/rambulance/railtie.rb
+++ lib/rambulance/railtie.rb
@@ -5,15 +5,27 @@ module Rambulance
         require "rambulance/exceptions_app"
       end
 
-      app.config.exceptions_app = ->(env) {
-        begin
-          ActiveSupport::Dependencies.load_missing_constant(Object, :ExceptionsApp)
-          ::ExceptionsApp.call(env)
-        rescue NameError
-          require "rambulance/exceptions_app" if !defined?(::Rambulance::ExceptionsApp)
-          ::Rambulance::ExceptionsApp.call(env)
+      app.config.exceptions_app =
+        if app.config.respond_to?(:autoloader) && app.config.autoloader == :classic
+          ->(env) {
+            begin
+              ActiveSupport::Dependencies.load_missing_constant(Object, :ExceptionsApp)
+              ::ExceptionsApp.call(env)
+            rescue NameError
+              require "rambulance/exceptions_app" if !defined?(::Rambulance::ExceptionsApp)
+              ::Rambulance::ExceptionsApp.call(env)
+            end
+          }
+        else
+          ->(env) {
+            begin
+              ::ExceptionsApp.call(env)
+            rescue NameError
+              require "rambulance/exceptions_app" if !defined?(::Rambulance::ExceptionsApp)
+              ::Rambulance::ExceptionsApp.call(env)
+            end
+          }
         end
-      }
 
       ActiveSupport.on_load(:after_initialize) do
         Rails.application.routes.append do
